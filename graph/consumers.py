a890377945222ec3327ecbe70196670574b88291
deviantcoder <andrewsafonovpost@gmail.com>
2024-12-20 17:42:34 +0200

    init commit

---
+++ graph/consumers.py
@@ -0,0 +1,38 @@
+import json
+from random import randint
+from asyncio import sleep
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+def get_day():
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    index = 0
+
+    # while True:
+    #     yield days[index]
+    #
+    #     index += 1
+    #
+    #     if index > 6:
+    #         index = 0
+
+    while True:
+        for day in days:
+            yield day
+
+
+class GraphConsumer(AsyncWebsocketConsumer):
+    def __init__(self):
+        super().__init__()
+        self.days_gen = get_day()
+
+    async def connect(self):
+        await self.accept()
+
+        for i in range(1000):
+            num = randint(-20, 20)
+            day = next(self.days_gen)
+
+            await self.send(json.dumps({'value': num, 'day': day}))
+            await sleep(1)
---
+++ graph/routing.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import consumers
+
+ws_urlpatterns = [
+    path('ws/graph/', consumers.GraphConsumer.as_asgi())
+]
